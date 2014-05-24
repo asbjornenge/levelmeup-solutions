35300c7a08e55ba1845927f8c61b0178faf88fd2
Asbjørn Enge <asbjorn@hanafjedle.net>
2014-05-24 15:51:46 +0200

    Working sol11

---
+++ sol11.js
@@ -0,0 +1,9 @@
+var level     = require('level')
+var sub       = require('level-sublevel')
+var db        = sub(level(process.argv[2]))
+
+var dinosaurs = db.sublevel('dinosaurs')
+var robots    = db.sublevel('robots')
+
+dinosaurs.put('slogan','rawr')
+robots.put('slogan','beep boop')
